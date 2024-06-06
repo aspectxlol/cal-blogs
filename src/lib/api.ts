f24b8a540927b1c4b377b4863344c4db2a6397ca
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-06 16:26:42 +0700

    add 404 thing so it doesnt break

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,20 +9,29 @@ export function getPostSlugs() {
   return fs.readdirSync(postsDirectory);
 }
 
-export function getPostBySlug(slug: string) {
+export function getPostBySlug(slug: string): Post | undefined {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const { data, content } = matter(fileContents);
 
-  return { ...data, slug: realSlug, content } as Post;
+  try {
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data, content } = matter(fileContents);
+    return { ...data, slug: realSlug, content } as Post;
+  } catch (error: any) {
+    if (error.code === 'ENOENT') {
+      console.error(`Post not found: ${fullPath}`);
+      return undefined; // Indicate post not found
+    } else {
+      throw error; // Re-throw other errors
+    }
+  }
 }
 
 export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
-    // sort posts by date in descending order
+    .filter((post): post is Post => post !== undefined) // Filter out undefined values
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
