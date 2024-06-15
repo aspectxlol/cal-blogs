78a8ad56918fdfe5e2b6148f45a71762b4636bdb
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-15 22:47:50 +0700

    perfect, might change later

---
+++ src/app/about-us/calvin/page.tsx
@@ -0,0 +1,11 @@
+export default async function Calvin() {
+  return  (
+    <main>
+      <section className="h-dvh w-full">
+        <div className="flex flex-col h-full justify-center">
+          <h1 className="text-center">Calvin</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
