24dfc53ad8439662e8dc154456ac76cb2e68b0bf
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-16 16:27:08 +0700

    move postPreview Component

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,8 +1,6 @@
+import PostPreview from "@/components/PostPreview";
 import { type Post } from "@/interfaces/Post";
-import { getAllPosts, getPostBySlug, getPostSlugs } from "@/lib/post";
-import { parseISO, format } from "date-fns";
-import Image from "next/image";
-import Link from "next/link";
+import { getAllPosts } from "@/lib/post";
 
 export default async function Post() {
   const allPost = getAllPosts()
@@ -16,38 +14,4 @@ export default async function Post() {
     </div>
   )
 
-}
-
-export const PostPreview = (
-  { post } : { post: Post }
-) => {
-  return (
-    <Link className="shadow-lg border-4 rounded" href={`/post/${post.slug}`}>
-      <Image
-        src={post.coverImage}
-        alt={post.title}
-        width={1300}
-        height={600}
-      />
-      <div className="p-4">
-        <h1 className="text-4xl font-bold">{post.title}</h1>
-        <div className="flex flex-row items-center m-2">
-          <Image
-            src={post.author.picture}
-            alt={post.author.name}
-            width={256}
-            height={256}
-            className="rounded-full w-12 h-12 mr-2"
-          />
-          <h1 className="text-2xl font-bold ml-2">{post.author.name}</h1>
-          <time
-            dateTime={post.date}
-            className="ml-6 items-center"
-          >
-            {format(parseISO(post.date), "LLLL d, yyyy")}
-          </time>
-        </div>
-      </div>
-    </Link>
-  )
 }
---
+++ src/components/PostPreview.tsx
@@ -0,0 +1,38 @@
+import { Post } from "@/interfaces/Post";
+import { format, parseISO } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PostPreview(
+  { post } : { post: Post }
+) {
+  return (
+    <Link className="shadow-lg border-4 rounded" href={`/post/${post.slug}`}>
+      <Image
+        src={post.coverImage}
+        alt={post.title}
+        width={1300}
+        height={600}
+      />
+      <div className="p-4">
+        <h1 className="text-4xl font-bold">{post.title}</h1>
+        <div className="flex flex-row items-center m-2">
+          <Image
+            src={post.author.picture}
+            alt={post.author.name}
+            width={256}
+            height={256}
+            className="rounded-full w-12 h-12 mr-2"
+          />
+          <h1 className="text-2xl font-bold ml-2">{post.author.name}</h1>
+          <time
+            dateTime={post.date}
+            className="ml-6 items-center"
+          >
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+        </div>
+      </div>
+    </Link>
+  )
+}
