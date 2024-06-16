292aefb433d651edf4773fa36876a825e2b93848
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-16 21:37:54 +0700

    make my site cooler, add things to calvins site

--- src/app/about-us/calvin/page.tsx
+++ src/app/about-us/calvin/page.tsx
@@ -2,8 +2,9 @@ export default async function Calvin() {
   return  (
     <main>
       <section className="h-dvh w-full">
-        <div className="flex flex-col h-full justify-center">
-          <h1 className="text-center">Calvin</h1>
+        <div className="flex flex-col h-full justify-center items-center">
+          <h1 className="text-center font-bold text-9xl">Calvin</h1>
+          <p className="text-center text-6xl max-w-6xl">Student, Friend, Therapist, Gacha Addict. Lore speaking, a dad and with 6 children. Play a whole ton of games and watches anime for life. Is tall just because he can.</p>
         </div>
       </section>
     </main>
--- src/app/about-us/louie/page.tsx
+++ src/app/about-us/louie/page.tsx
@@ -1,17 +1,25 @@
-import LouieStyles from '@/styles/Louie.module.css'
+"use client"
 
-export default async function Louie() {
+import LouieStyles from '@/styles/Louie.module.css'
+import { createRef } from 'react'
+
+export default function Louie() {
+  const sectRef = createRef<HTMLElement>()
+
   return (
     <main>
       <section className="h-dvh w-full bg-black text-white">
         <div className="flex flex-col h-full justify-center">
-          <h1 className="text-center text-9xl font-bold">Louie</h1>
+          <h1 className="text-center text-9xl font-bold ">Louie</h1>
+        </div>
+        <div className='absolute bottom-16 left-0 right-0 mx-auto items-center justify-center flex'>
+          <button className='rounded-full p-5 border-white border-4 hover:mb-4 hover:p-7 transition-all' onClick={() => { sectRef.current?.scrollIntoView({ behavior: "smooth" }) }}>Look At More!</button>
         </div>
       </section>
-      <section className="h-dvh w-full">
+      <section className="h-dvh w-full" ref={sectRef}>
         <div className="flex flex-col h-full justify-center text-center">
           <div className="flex flex-row justify-center">
-            <h1 className='text-9xl font-extrabold'>The Worlds best Programmer</h1>
+            <h1 className='text-9xl font-extrabold'>The Worlds best <span className='bg-red-800 text-white'>Programmer</span></h1>
           </div>
         </div>
       </section>
