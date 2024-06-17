566b54551014c7704d34bbb28863fa8847e465ca
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-17 20:02:23 +0700

    fix problem

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { getAllPosts } from "@/lib/post";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request | NextRequest, res: Response | NextResponse) {
   // return res.json(await getAllPosts());
   // console.log("AA")
   return Response.json(await getAllPosts())
