eef9f3f1b4eaf2c9c3d65657d421a155871172ae
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-17 19:06:32 +0700

    About us Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { density } from "@/constants";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createRef } from "react";
 import SkyCSS from '@/styles/sky.module.css'
+import Link from "next/link";
 
 interface Circle {
   size: number;
@@ -16,7 +17,21 @@ interface Circle {
 
 export default function Home() {
   const [circles, setCircles] = useState<Circle[]>([]);
-  
+  const sectRef = createRef<HTMLElement>()
+  const [InView, setInView] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting)
+      },
+      { rootMargin: "-300px" }
+    );
+    observer.observe(sectRef.current!)
+    return () => observer.disconnect()
+  }, [sectRef]);
+
+
   useEffect(() => {
     const createCircle = (): Circle => {
       const size = Math.random() * 3 + 1; 
@@ -52,6 +67,7 @@ export default function Home() {
       circles.forEach((circle) => clearInterval(circle.intervalId));
       window.removeEventListener('resize', handleResize);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -72,6 +88,20 @@ export default function Home() {
           />
         ))}        
         <h1 className="font-bold text-3xl md:text-7xl">Calvin & Louie</h1>
+        <div className='absolute bottom-16 left-0 right-0 mx-auto items-center justify-center flex'>
+          <button className='rounded-full p-5 border-white border-4 hover:mb-4 hover:p-7 transition-all' onClick={() => { sectRef.current?.scrollIntoView({ behavior: "smooth" }) }}>Look At More!</button>
+        </div>
+      </section>
+      <section className="h-dvh w-full p-5" ref={sectRef}>
+        <h1 className={`text-9xl text-center font-bold mt-52 block ${InView ? "translate-x-0 opacity-100" : "-translate-x-48 opacity-0"} transition-all duration-500`}>About Us</h1>
+        <div className="flex flex-row justify-center text-center items-center my-14 gap-5">
+          <Link className={`rounded-lg shadow-lg p-12 border-2 hover:shadow-xl transition-all ${InView ? "translate-x-0 opacity-100" : "-translate-x-48 opacity-0"} transition-all duration-500 delay-75`} href={"/about-us/louie"}>
+            <h1 className="font-bold text-2xl">Louie</h1>
+          </Link>
+          <Link className={`rounded-lg shadow-lg p-12 border-2 hover:shadow-xl transition-all ${InView ? "translate-x-0 opacity-100" : "-translate-x-48 opacity-0"} transition-all duration-500 delay-150`} href={"/about-us/calvin"}>
+            <h1 className="font-bold text-2xl">Calvin</h1>
+          </Link>
+        </div>
       </section>
     </main>
   );
