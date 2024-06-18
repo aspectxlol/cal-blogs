eb4b098c40db4da7bac8f0fd68f90b90a2e9280a
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-18 16:38:35 +0700

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,12 @@ import NavbarCSS from '@/styles/Navbar.module.css'
 export default function Navbar() {
   return (
     <nav>
-      <div className="fixed flex flex-row justify-center items-center text-white font-extrabold w-full text-4xl p-5">
+      <div className="fixed flex flex-row justify-center items-center text-white font-extrabold w-full text-4xl">
         <div className="flex flex-row gap-16">
-          <Link href={'/post'} className={`rounded hover:-translate-y-4 hover:shadow-xl transition-all p-5 ${NavbarCSS['link']} duration-150`}>
+          <Link href={'/post'} className={`rounded hover:-translate-y-4 hover:shadow-xl transition-all p-2 ${NavbarCSS['link']} duration-150`}>
             <h1>Blogs</h1>
           </Link>
-          <Link href={'/store'} className={`rounded hover:-translate-y-4 hover:shadow-xl transition-all p-5 ${NavbarCSS['link']} duration-150`}>
+          <Link href={'/store'} className={`rounded hover:-translate-y-4 hover:shadow-xl transition-all p-2 ${NavbarCSS['link']} duration-150`}>
             <h1>store</h1>
           </Link>
         </div>
